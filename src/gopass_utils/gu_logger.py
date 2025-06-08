32335cfc32d0add79f036036262f03c5919b7851
Rick <ricks.homelab@gmail.com>
2025-06-09 07:01:07 +0800

    minimal logger

--- src/gopass_utils/gu_logger.py
+++ src/gopass_utils/gu_logger.py
@@ -22,70 +22,24 @@ __docformat__ = 'reStructuredText'
 # pylint: disable=no-member
 
 import logging
-import sys
-import os
-from logging.handlers import RotatingFileHandler
 
-
-
-def configure_logger(name: str = "",
-                     log_level: int = logging.INFO,
-                     log_dir: str = './',
-                     log_file: str = __name__,
-                     stream: bool = True) -> logging.Logger:
-    """
-    Configures and returns a logger with the specified name and log level.
-
-    :param name: Name of the logger (usually __name__).
-    :param log_level: Logging level (e.g., logging.DEBUG, logging.INFO).
-    :param log_dir: Directory where log files are stored.
-    :param log_file: Full pathname of log file (usually __name__).
-    :param stream: Configure stream logger if True.
-    :return: Configured logger instance.
-    """
-    # Ensure the log directory exists
-    if log_dir != './':
-        try:
-            os.makedirs(log_dir, exist_ok=True)
-        except OSError:
-            print("Configuration Error: Unable to create log directory at {}".format(log_dir))
-            sys.exit(-1)
-
-    # Create the logger
+def configure_logger(name: str = "", log_level: int = logging.INFO, stream: bool = True) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
 
-    # Avoid duplicate handlers if the logger is already configured
     if logger.hasHandlers():
         return logger
 
-    # Define log format
-    log_format_stream = logging.Formatter(
+    formatter = logging.Formatter(
         "[%(asctime)s] %(levelname)s [%(module)s:%(lineno)d] - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S"
     )
-    log_format_file = logging.Formatter(
-        "[%(asctime)s] %(levelname)s [%(module)s:%(lineno)d] %(funcName)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S"
-    )
 
-    # Console handler
     if stream:
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(log_format_stream)
-        logger.addHandler(console_handler)
+        console = logging.StreamHandler()
+        console.setFormatter(formatter)
+        logger.addHandler(console)
 
-    # Rotating file handler
-    file_handler = RotatingFileHandler(
-        filename=log_file,
-        maxBytes=100 * 1024 * 1024,  # 100 MB
-        backupCount=15  # Keep 15 backup files
-    )
-    file_handler.setFormatter(log_format_file)
-    #logger.addHandler(file_handler)
-    logger.info('Logger %s initialized.', name)
-
-    logger.propagate = True
-
+    logger.propagate = False
+    logger.info("Logger %s initialized", name)
     return logger
-
